0aa998eb763d52e81dbd8d5d13513c32e9c4dff5
Sergio Leon <sergiom.leon@gmail.com>
2019-01-22 20:16:17 -0800

    =database

--- database/migrations/2019_01_19_063324_create_car_services_table.php
+++ database/migrations/2019_01_19_063324_create_car_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarServiceExtrasTable extends Migration
+class CreateCarServicesTable extends Migration
 {
     /**
      * Run the migrations.
